a5fbf854c3835cfee077a8498f2af623c24f5971
Yoshi <wakaharayoshitaka@gmail.com>
2020-02-10 22:39:17 +0900

    4-AddChatterResponse tmp

--- index.js
+++ index.js
@@ -2,15 +2,16 @@
 const router = require('./lib/router');
 
 //ToDo change func name
-function getResponse(req) {
+function getResponse(req, userID) {
   //ToDo Delete
-  console.log(req);
-  return router.route(req);
+  //console.log(req);
+  return router.route(req, userID);
 }
 //ToDo Delete
 //getResponse('hi');
-//getResponse('weather');
-getResponse('chatter');
+getResponse('weather');
+//getResponse('chatter');
+//getResponse('cute');
 
 module.exports = {
   getResponse
